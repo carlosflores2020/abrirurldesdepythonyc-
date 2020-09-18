89678b5db3977e55c9aa6ef371a5b3052d1f8ac7
carlos flores <64626375+carlosflores2020@users.noreply.github.com>
2020-09-18 14:04:17 -0300

    Add files via upload

---
+++ abrir url.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+//COMO ABRI UNA URL DESDE C++
+using namespace std;
+
+int main()
+{
+	cout<<"Presione enter para abrir https://autogestion.guarani.unc.edu.ar/"<<endl;
+	system ("pause<null");
+	system("start https://autogestion.guarani.unc.edu.ar/");
+	cout<<"Preione enter para salir"<<endl;
+	system("pause>null");
+	return 0;
+}
---
+++ web.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+import time
+
+print("El programa inicio a las" +time.ctime())
+
+time.sleep (10)
+    
+webbrowser.open("https://www.youtube.com/watch?v=m2nscBtQEIs")
